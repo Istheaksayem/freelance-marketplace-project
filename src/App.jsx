78b669fce2796e286b28e6ef2719b70eb280e815
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-08 21:14:28 +0600

    start my freelance MarketPlace project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-3xl text-red-600'>freelance marketPlace </h1>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './Layout/RootLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AllJobs from './components/AllJobs/AllJobs.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+   Component:RootLayout,
+   children:[
+    {
+      index:true,
+      Component:Home
+    },
+    {
+      path:"allJobs",
+      Component:AllJobs
+    }
+   ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+ <RouterProvider router={router} />,
+  </StrictMode>,
+)
